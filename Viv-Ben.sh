0641a9cfd4b308e66e125285b2fdb9dd2160b935
VivBen <ndungbowo@gmail.com>
2022-02-20 20:06:19 -0500

    man

---
+++ Viv-Ben.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Hello love
+echo "enter your name"
+read name
+echo "enter password"
+read password
+date
---
+++ Viv_Ben4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Hello
+echo "Enter name"
+read name
+echo "Enter password"
+date
+whoami
